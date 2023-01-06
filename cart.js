b1e3a2e3b3932dfd482e73cef6bc4688dbe76726
Sameeksha2002 <sameekshadhoundiyal@gmail.com>
2023-01-06 20:28:19 +0530

    item page done

---
+++ cart.js
@@ -0,0 +1,23 @@
+// function getDatas(i) {
+
+//     let text = document.getElementsByClassName('card-text');
+//     let image = document.querySelectorAll(".card-img-top");
+
+//     let title = text[i].textContent;
+
+//     console.log(title);
+//     sessionStorage.setItem("title", title);
+//     location.href = "item.html";
+// }
+
+
+
+// function setDatas() {
+//     let itemName = document.getElementById('nameItem');
+
+//     var title2 = sessionStorage.getItem("title");
+//     itemName.textContent = title2;
+//     console.log(title2);
+
+
+// }
